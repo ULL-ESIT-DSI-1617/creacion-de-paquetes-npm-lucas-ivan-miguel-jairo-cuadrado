2513b5bfdee23eebf09b4e560c5a4d7dfa45bbae
alu0100785265 <alu0100785265@ull.edu.es>
2017-04-21 17:44:53 +0000

    añadiendo travis

--- ull-shape-lucas-ivan-miguel-jairo-quare.js
+++ ull-shape-lucas-ivan-miguel-jairo-quare.js
@@ -1,11 +1,21 @@
 "use strict"
+/*
+     Se require el paquete que contiene la clase Shape.
+*/
 var nuevo = require("ull-shape-lucas-ivan-miguel-jairo");
 var Shape = nuevo.Shape;
+/**
+ * Clase para representar un Cuadrado que es de tipo Shape.
+ *
+ * @param {object} options Bloque de código.
+ * @param {width} options.width Anchura de la figura.
+ */
 class Square extends Shape {
 
     constructor(options) {
 
       super(options)
+        /* @param {function?} done Callback function to execute when finished.*/
 
     }
 
